5bf2020022c0c46d36ae963e0c1e83b778ae0a1b
ANANT PATEL <120110880+ANANTPATEL505@users.noreply.github.com>
2025-11-23 15:52:30 +0530

    Update 01 hello.rs

--- 01 hello.rs
+++ 01 hello.rs
@@ -1,6 +1,9 @@
 fn main() {
     println!("Hello, anant");
 
-    let num:u8 = 255;
+    let mut num:u8 = 255;
+    println!("The value of num is: {}", num);
+
+    num=30;
     println!("The value of num is: {}", num);
 }
