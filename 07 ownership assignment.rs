3ad2710ef51b2633b79ef97474853a39b1037cea
ANANT PATEL <120110880+ANANTPATEL505@users.noreply.github.com>
2025-11-29 12:08:14 +0530

    Add ownership assignment example in Rust
    Implement ownership transfer examples in Rust.

---
+++ 07 ownership assignment.rs
@@ -0,0 +1,18 @@
+fn main(){
+
+    let s1:String=String::from getstring(); // 3. s1 is the ownerof hello
+    println!("this is s1 : {}",s1);
+
+    let s2:String=String::from("world");            // 4. s2 is the owner of world
+    let s3:String=String::from send_get_string(s2); // 5. transferring the ownership of s2 to send_get_string(s)
+    println!("this is s3 : {}",s3);                 // 7. s2 is the new owner of world
+}
+
+fn getstring()->String{
+    let new:String+String::from("hello"); // 1. new is the owner of hello
+    return new;                           // 2. transfering the ownership
+}
+
+fn send_get_string(s:String)->String{
+    return s;  // 6. transferring the ownership back to the caller
+}
